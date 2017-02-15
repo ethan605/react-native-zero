0eef62cac592c2019fcbb7213443fb1e9e3e8329
Ethan N <thanhnx.605@gmail.com>
2017-02-15 16:50:24 +0700

    Add devDependencies packages

---
+++ gulp-helpers/codepush/helpers.js
@@ -0,0 +1,106 @@
+/**
+ * CodePush helpers
+ */
+/* eslint-disable no-console */
+
+// Gulp modules
+const gulp = require('gulp');
+const shell = require('gulp-shell');
+
+// Helper modules
+const fs = require('fs');
+const lodash = require('lodash');
+const yargs = require('yargs');
+
+const DRY_RUN = !!yargs.argv.dry;
+
+const CODEPUSH_CONFIGS_FILE = './gulp-helpers/codepush/configs.json';
+const CODEPUSH_RELEASE_DIR = './CodePushRelease';
+const CODEPUSH_BUNDLE_FILE = {
+  android: 'index.android.bundle',
+  ios: 'main.jsbundle',
+};
+const CODEPUSH_ENTRY_FILE = {
+  ios: 'index.ios.js',
+  android: 'index.android.js',
+};
+
+function readConfigs() {
+  const content = fs.readFileSync(CODEPUSH_CONFIGS_FILE);
+  const {
+    app_name: appName,
+    build_number: buildNumber,
+    build_target: target,
+    version_name: versionName,
+  } = JSON.parse(content);
+  
+  return {
+    appName,
+    buildNumber,
+    target,
+    versionName,
+  };
+}
+
+function prepareCommand({ platform }) {
+  const options = [
+    { flag: 'assets-dest', value: CODEPUSH_RELEASE_DIR },
+    { flag: 'bundle-output', value: `${CODEPUSH_RELEASE_DIR}/${CODEPUSH_BUNDLE_FILE[platform]}` },
+    { flag: 'dev', value: false },
+    { flag: 'entry-file', value: CODEPUSH_ENTRY_FILE[platform] },
+    { flag: 'platform', value: platform },
+  ];
+
+  const command = 'react-native bundle';
+  const compiledOptions = options.map(({ flag, value }) => `--${flag} ${value}`).join(' ');
+
+  return `${command} ${compiledOptions}`;
+}
+
+function uploadCommand(configs) {
+  const { appName, buildNumber, target, versionName } = configs;
+  
+  const deploymentName = lodash.capitalize(target);
+  const readableVersion = `${deploymentName} ${versionName} build-${buildNumber}`;
+  const description = `Universal iOS & Android update (${readableVersion})`;
+
+  const params = [
+    appName,
+    CODEPUSH_RELEASE_DIR,
+    `~${versionName}`
+  ];
+
+  const options = [
+    { flag: 'deploymentName', value: deploymentName },
+    { flag: 'description', value: `'${description}'` },
+    { flag: 'disabled', value: target === 'release' },
+  ];
+
+  const command = 'code-push release';
+  const compiledParams = params.join(' ');
+  const compiledOptions = options.map(({ flag, value }) => `--${flag} ${value}`).join(' ');
+
+  return `${command} ${compiledParams} ${compiledOptions}`;
+}
+
+function execCommands(...commands) {
+  if (DRY_RUN)
+    console.log(['Commands to be executed:\n', ...commands, '\n'].join('\n'));
+  else
+    gulp.src('.').pipe(shell(commands));
+}
+
+module.exports = {
+  execCommands,
+  prepareCommand,
+  readConfigs,
+  uploadCommand,
+  constants: {
+    CODEPUSH_CONFIGS_FILE,
+    CODEPUSH_RELEASE_DIR,
+    CODEPUSH_BUNDLE_FILE,
+    CODEPUSH_ENTRY_FILE,
+  },
+};
+
+/* eslint-enable no-console */
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// Gulp modules
+const gulp = require('gulp');
+const sequence = require('gulp-sequence');
+
+const {
+  execCommands,
+  prepareCommand,
+  readConfigs,
+  uploadCommand,
+  constants: {
+    CODEPUSH_RELEASE_DIR,
+  },
+} = require('./gulp-helpers/codepush/helpers');
+
+/**
+ * CodePush tasks
+ */
+gulp.task('codepush:clean', () => (
+  // Clean CodePush release folder & rebuild typescript sources
+  execCommands(
+    `rm -rf ${CODEPUSH_RELEASE_DIR}`,
+    `mkdir -p ${CODEPUSH_RELEASE_DIR}`
+  )
+));
+
+gulp.task('codepush:prepare', () => (
+  // Prepare bundle files for both Android & iOS
+  execCommands(
+    prepareCommand({ platform: 'android' }),
+    prepareCommand({ platform: 'ios' })
+  )
+));
+
+gulp.task('codepush:upload', () => (
+  // Upload to CodePush server
+  execCommands(uploadCommand(readConfigs()))
+));
+
+gulp.task('codepush:release', sequence(
+  'codepush:clean',
+  'codepush:prepare',
+  'codepush:upload'
+));
