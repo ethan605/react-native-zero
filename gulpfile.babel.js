02872d24e24906dbd6acbfa0e7a1f46ba9bf06a1
Ethan Nguyen <thanhnx.605@gmail.com>
2017-02-21 10:11:57 +0700

    Refine

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,9 @@
 // Gulp modules
 import gulp from 'gulp';
-import debug from 'gulp-debug';
+// import debug from 'gulp-debug';
 
 // Helpers
 import './gulp-helpers/codepush';
 import './gulp-helpers/zero';
 
-gulp.task('exp', () => (
-  gulp.src('./ios/*')
-    .pipe(debug({ title: '' }))
-    // .pipe(gulp.dest('./__cloned__/ios'))
-));
+gulp.task('exp', () => {});
