f0506e818eb9d685465dcc19039246e5aa82e182
Ethan Nguyen <thanhnx.605@gmail.com>
2017-02-28 18:19:08 +0700

    Configure Redux store to work with redux-devtools-extensions & react-native-debugger

--- app/redux/store/index.js
+++ app/redux/store/index.js
@@ -7,6 +7,11 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import reducers from '../reducers';
 
+/**
+ * Configure to work with `redux-devtools-extensions` & `react-native-debugger`
+ * https://github.com/jhen0409/react-native-debugger
+ * https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+ */
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default createStore(
