b904c226cd0b26b11df730828e7c187c5bde971d
Ethan Nguyen <thanhnx.605@gmail.com>
2017-03-03 15:15:59 +0700

    Add Firebase frameworks

--- ios/ZeroProj/AppDelegate.m
+++ ios/ZeroProj/AppDelegate.m
@@ -13,34 +13,68 @@
 #import <React/RCTRootView.h>
 
 #import <CodePush/CodePush.h>
+@import FirebaseCore;
+#import "RNFIRMessaging.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  NSURL *jsCodeLocation;
+  [self configureFIR];
   
 #ifdef DEBUG
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings]
-                    jsBundleURLForBundleRoot:@"index.ios"
-                    fallbackResource:nil];
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings]
+                           jsBundleURLForBundleRoot:@"index.ios"
+                           fallbackResource:nil];
 #else
-  jsCodeLocation = [CodePush bundleURL];
+  NSURL *jsCodeLocation = [CodePush bundleURL];
 #endif
-
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ZeroProj"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+  [RNFIRMessaging willPresentNotification:notification withCompletionHandler:completionHandler];
+}
+
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)())completionHandler {
+  [RNFIRMessaging didReceiveNotificationResponse:response withCompletionHandler:completionHandler];
+}
+
+- (void)application:(UIApplication *)application
+didReceiveRemoteNotification:(nonnull NSDictionary *)userInfo
+fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler {
+  [RNFIRMessaging didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
+}
+
+- (void)configureFIR {
+#ifdef DEBUG
+  NSString *configsName = @"GoogleService-Info.dev";
+#else
+#ifdef STAGING
+  NSString *configsName = @"GoogleService-Info.stg";
+#else
+  NSString *configsName = @"GoogleService-Info";
+#endif
+#endif
+  
+  NSString *configsPath = [[NSBundle mainBundle] pathForResource:configsName ofType:@"plist"];
+  FIROptions *firebaseOptions = [[FIROptions alloc] initWithContentsOfFile:configsPath];
+  [FIRApp configureWithOptions:firebaseOptions];
 }
 
 @end
